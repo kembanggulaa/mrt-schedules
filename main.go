308e3b68fdef6110434a6573982f65f961935c0e
= <=>
2025-09-15 23:47:59 +0700

    Add gin framework, go.mod, and go.sum files for dependency management.

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	var router = gin.Default()
+
+	router.Run(":8080")
 }
